3e73844be8fae2b40d0b1fcc8c39c1df114af756
Jo Shields <joshield@microsoft.com>
2023-09-25 10:08:45 -0400

    Build.cmd build fails if -build is passed twice, so deduplicate

--- Build.cmd
+++ Build.cmd
@@ -1,2 +1,9 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" -build %*"
+setlocal enabledelayedexpansion
+
+rem Force build to always be set
+set EXTRAARGS=
+echo %*|find "-build"
+if ERRORLEVEL 1 (set EXTRAARGS=!EXTRAARGS! -build)
+
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" %EXTRAARGS% %*"
